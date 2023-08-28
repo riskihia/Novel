95d9ec7ab46ab48be2bc8026b149a6576d225c5c
Riski Rahmat Hia <riskirahmathia3@gmail.com>
2023-08-28 19:06:57 +0700

    clear tailwindforms error

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
+import tailwindForms from "@tailwindcss/forms";
 
 export default defineConfig({
     plugins: [
-        require("@tailwindcss/forms"),
+        tailwindForms,
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
